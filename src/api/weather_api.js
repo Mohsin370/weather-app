8870f21828e90ef754bdafbd8a63c577f6b98293
mohsin370 <mohsinijaz13@gmail.com>
2021-02-09 19:57:30 +0500

    basic app created

--- src/api/weather_api.js
+++ src/api/weather_api.js
@@ -3,13 +3,17 @@ import Axios from 'axios'
 class Api {
 
     weather_by_city = (city_name) => {
-        Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+        let res = Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
             .then((res) => {
-                console.log(res)
+                return res.data;
             })
+
+        return res;
     }
     weather_by_Cords = async(lat, lon) => {
-        let res = await Axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+        let res = await Axios.get(`
+        
+        https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
             .then((res) => {
                 return res.data;
             })
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,66 +1,125 @@
 import React, { Component } from 'react';
 import Api from '../api/weather_api';
-import WeatherCard from '../components/weatherCard';
-import './index.css'
+import './index.css';
+import sunnyImage from '../images/sunny.jpg';
+import HazeImage from '../images/haze.jpg'
+import rainImage from '../images/rain.jpeg'
+import HazeIcon from '../images/icons/haze_icon.png';
+import SunIcon from '../images/icons/sun_icon.png';
+import SearchIcon from '../images/icons/search.png';
 
+
+
+let latLngApi = new Api();
 
 class Index extends Component {
 
-
-    state={
-        sevenDaysWeather:[]
+    state = {
+        currentWeatherBasic: {},
+        cityName: ''
     }
-    componentDidMount(){
+
+
+    componentDidMount() {
         this.getCurrentWeatherByLoc();
     }
 
-     getCurrentWeatherByLoc =async ()=>{
+    getCurrentWeatherByLoc = () => {
 
-        let latLngApi = new Api();
         let weatherStatus = ""
-        navigator.geolocation.getCurrentPosition( async function (position) {
-            weatherStatus =  await latLngApi.weather_by_Cords(position.coords.latitude,position.coords.longitude);
-            console.log(weatherStatus);
-          });
-
         var d = new Date();
-        var currentDay = d.getDay();
-          let weatherStateArray=[]
-          var weekday = new Array(7);
-          weekday[0] = "Sunday";
-          weekday[1] = "Monday";
-          weekday[2] = "Tuesday";
-          weekday[3] = "Wednesday";
-          weekday[4] = "Thursday";
-          weekday[5] = "Friday";
-          weekday[6] = "Saturday";
-
-          weekday.forEach((res,index)=>{
-              if(currentDay>6){
-                  currentDay = 0;
-              }
-              console.log(weekday[currentDay]);
-              weatherStateArray.push(
-                  {dayName: weekday[currentDay], WeatherStatus: weatherStatus }
-                  )
-              currentDay+=1;
-          })
-
-          this.setState({
-              sevenDaysWeather:weatherStateArray,
-          })
+        console.log(d.toLocaleTimeString())
+        let weekdays = [];
+        weekdays[2] = 'Tuesday';
+        weekdays[1] = 'Monday';
+        weekdays[3] = 'Wednesday';
+        weekdays[4] = 'Thursday';
+        weekdays[5] = 'Friday';
+        weekdays[6] = 'Saturday';
+        weekdays[7] = 'sunday';
 
 
-        
+        let promise = new Promise((res, rej) => {
+
+            navigator.geolocation.getCurrentPosition(async function (position) {
+                weatherStatus = await latLngApi.weather_by_Cords(position.coords.latitude, position.coords.longitude);
+                // weatherStatus.weather[0].main ="Rain";
+                let backgroundImage,icon;
+                if(weatherStatus.weather[0].main == "Haze" || "Smoke"){
+                    backgroundImage = HazeImage;
+                    icon =HazeIcon;
+                }else if(weatherStatus.weather[0].main == "Sunny"){
+                    backgroundImage = sunnyImage;
+                    icon =SunIcon;
+                }else if (weatherStatus.weather[0].main == "Rain"){
+                    backgroundImage = rainImage;
+                    icon =SunIcon;
+                }
+
+
+                let currentWeather = {
+                    tempInCel: ((weatherStatus.main.temp - 273.15)),
+                    weatherCondition: weatherStatus.weather[0].main,
+                    Day: weekdays[d.getDay()],
+                    Time: d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+                    cityName: "Current Location",
+                    Icon:icon,
+                    backgroundImage: backgroundImage,
+
+
+                }
+                // console.log(weatherStatus)
+                res(currentWeather);
+            });
+        })
+        promise.then((res) => {
+            console.log(res);
+            this.setState({ currentWeatherBasic: res })
+
+        })
     }
+
+
+    getWeatherbyCity = async () => {
+        let weatherStatus = await latLngApi.weather_by_city(this.state.cityName);
+        console.log(weatherStatus);
+
+    }
+
     render() {
         return (
-            <div className="weather_cards">
-                {this.state.sevenDaysWeather.map((res)=>{
-                    console.log(res);
-                    return<WeatherCard dayName={res.dayName}></WeatherCard>
+            <div className="weather_main" style={{ backgroundImage: `url(${this.state.currentWeatherBasic.backgroundImage})` }}>
 
-                })}
+                <div className="temprature_container">
+                    <div className="container_item_temprature">
+                        <p className="temprature">{this.state.currentWeatherBasic.tempInCel}
+                            <span>&#176;</span>
+                        </p>
+
+                        <div className="city_date_container">
+                            <p className="city"> {this.state.currentWeatherBasic.cityName}   </p>
+                            <p className="day_time"> {this.state.currentWeatherBasic.Day} - {this.state.currentWeatherBasic.Time}</p>
+                        </div>
+
+                        <div className="city_date_container">
+                            <img className="weather_icon" src={this.state.currentWeatherBasic.Icon} alt="haze_icon"/>
+                            <p className="weather_condition"> {this.state.currentWeatherBasic.weatherCondition}</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="weather_details">
+
+                    <div className="weather_detals_main">
+                    <input placeholder="Enter City Name"
+                    onChange={(e) => { this.setState({ cityName: e.target.value }) }} className="searchInput"></input>
+                    <button onClick={this.getWeatherbyCity} className="searchBtn">
+                        <img src={SearchIcon} alt="search" className="searchIcon"/>
+                    </button>
+                    </div>
+                </div>
+
             </div>
 
         )
